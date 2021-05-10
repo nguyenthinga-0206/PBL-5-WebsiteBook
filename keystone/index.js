a373b069643d656ce0e5d33cd77a35f1c8d0011c
Robert Tran <truongduchuy910@gmail.com>
2021-05-10 23:27:24 +0700

    remote database config

--- keystone/index.js
+++ keystone/index.js
@@ -1,50 +1,71 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
-const { Text, Checkbox, Password, Integer, Color, Relationship, Float, Select, DateTime, CloudinaryImage, File } = require('@keystonejs/fields');
-const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-const initialiseData = require('./initial-data');
-const { LocalFileAdapter } = require('@keystonejs/file-adapters');
-const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
-const PROJECT_NAME = 'first-app';
-const adapterConfig = { mongoUri: 'mongodb://localhost/first-app' };
-const UserSchema = require('./models/Users');
-const BaivietSchema = require('./models/Baiviet');
-const ChitietdonhangSchema = require('./models/Chitietdonhang');
-const DonhangSchema = require('./models/Donhang');
-const GiohangSchema = require('./models/Giohang');
-const ImgSchema = require('./models/Img');
-const NhacungcapSchema = require('./models/Nhacungcap');
-const PhanloaisachSchema = require('./models/Phanloaisach');
-const PhieunhapsachSchema = require('./models/Phieunhapsach');
-const SachSchema = require('./models/Sach');
+const { Keystone } = require("@keystonejs/keystone");
+const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
+const {
+  Text,
+  Checkbox,
+  Password,
+  Integer,
+  Color,
+  Relationship,
+  Float,
+  Select,
+  DateTime,
+  CloudinaryImage,
+  File,
+} = require("@keystonejs/fields");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const initialiseData = require("./initial-data");
+const { LocalFileAdapter } = require("@keystonejs/file-adapters");
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
+const PROJECT_NAME = "first-app";
+const adapterConfig = {
+  mongoUri: "mongodb://cnw:concobebe123@db.itoa.vn:27017/cnw",
+};
+const UserSchema = require("./models/Users");
+const BaivietSchema = require("./models/Baiviet");
+const ChitietdonhangSchema = require("./models/Chitietdonhang");
+const DonhangSchema = require("./models/Donhang");
+const GiohangSchema = require("./models/Giohang");
+const ImgSchema = require("./models/Img");
+const NhacungcapSchema = require("./models/Nhacungcap");
+const PhanloaisachSchema = require("./models/Phanloaisach");
+const PhieunhapsachSchema = require("./models/Phieunhapsach");
+const SachSchema = require("./models/Sach");
 
 const fileAdapter = new LocalFileAdapter({
-  src: './file',
-  path: './file',
-
+  src: "./file",
+  path: "./file",
 });
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
-  onConnect: process.env.CREATE_TABLES !== 'true' && initialiseData,
+  onConnect: process.env.CREATE_TABLES !== "true" && initialiseData,
+  secureCookies: process.env.NODE_ENV === "production",
+  cookieSecret: "conco",
+  cookie: {
+    //https://www.keystonejs.com/keystonejs/keystone/#createauthstrategyconfig
+    secure: process.env.NODE_ENV === "production", // Default to true in production
+    maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+    sameSite: false,
+  },
 });
 
-keystone.createList('User', UserSchema);
-keystone.createList('Baiviet', BaivietSchema);
-keystone.createList('Chitietdonhang', ChitietdonhangSchema);
-keystone.createList('Donhang', DonhangSchema);
-keystone.createList('Giohang', GiohangSchema);
-keystone.createList('IMG', ImgSchema);
-keystone.createList('Nhacungcap', NhacungcapSchema);
-keystone.createList('Phanloaisach', PhanloaisachSchema);
-keystone.createList('Sach', SachSchema);
-keystone.createList('Phieunhapsach', PhieunhapsachSchema);
+keystone.createList("User", UserSchema);
+keystone.createList("Baiviet", BaivietSchema);
+keystone.createList("Chitietdonhang", ChitietdonhangSchema);
+keystone.createList("Donhang", DonhangSchema);
+keystone.createList("Giohang", GiohangSchema);
+keystone.createList("IMG", ImgSchema);
+keystone.createList("Nhacungcap", NhacungcapSchema);
+keystone.createList("Phanloaisach", PhanloaisachSchema);
+keystone.createList("Sach", SachSchema);
+keystone.createList("Phieunhapsach", PhieunhapsachSchema);
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
-  list: 'User',
-  config: { protectIdentities: process.env.NODE_ENV === 'production' },
+  list: "User",
+  config: { protectIdentities: process.env.NODE_ENV === "production" },
 });
 
 module.exports = {
@@ -58,4 +79,3 @@ module.exports = {
     }),
   ],
 };
-
