a0c63008de26daf39a06364c7f11ad6194e038a1
nguyenthinga-0206 <nga3421@gmail.com>
2021-05-13 19:54:31 +0700

    update

--- keystone/giaodien/components/Img/Item/UI.js
+++ keystone/giaodien/components/Img/Item/UI.js
@@ -1,9 +1,7 @@
-import { Image } from "@chakra-ui/react";
 
 export default function UI({image}){
     return<div>
-       <Image src={image.publicUrl} alt="Segun Adebayo"/>
-       {/* <img src={image.publicUrl} alt="Segun Adebayo"/> */}
+       <img src={image.publicUrl} alt="Image"/>
         </div>
 
 }
