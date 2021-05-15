7621d859a1e1d643f05b2ad88145e55dc79e1073
Robert Tran <truongduchuy910@gmail.com>
2021-05-15 22:33:48 +0700

    save

--- keystone/giaodien/components/User/Item/UI.js
+++ keystone/giaodien/components/User/Item/UI.js
@@ -1,9 +1,5 @@
+import HTML from "../../HTML";
 
-export default function UI({user}){
-    return<div>
-        <p>{user.name}</p>
-        <p>{user.email}</p>
-        <p>{(user.isAdmin&&user.password_is_set)?<p>Welcome admin</p>:<p>Not Admin</p>}</p>
-        </div>
-
+export default function UI({ user }) {
+  return HTML.generation(user);
 }
