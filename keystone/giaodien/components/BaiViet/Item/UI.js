1596fb770c247072c77270ccbb4217599820de7b
Robert Tran <truongduchuy910@gmail.com>
2021-05-15 22:31:31 +0700

    save

--- keystone/giaodien/components/BaiViet/Item/UI.js
+++ keystone/giaodien/components/BaiViet/Item/UI.js
@@ -1,8 +1,5 @@
+import HTML from "../../HTML";
 
-export default function UI({baiViet}){
-    return<div>
-        <p>{baiViet.tieude}</p>
-        <p>{baiViet.baiViet}</p>
-        </div>
-
+export default function UI({ baiViet }) {
+  return HTML.generation(baiViet);
 }
