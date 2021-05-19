0b6e71594eaad6b6cfc867ea403220aa5df610b5
nguyenthinga-0206 <nga3421@gmail.com>
2021-05-19 23:35:23 +0700

    update

--- keystone/giaodien/pages/_app.js
+++ keystone/giaodien/pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
     <ApolloProvider client={apolloClient}>
       <CartProvider>
         <ChakraProvider>
+        
           <Component {...pageProps} />
         </ChakraProvider>
       </CartProvider>
