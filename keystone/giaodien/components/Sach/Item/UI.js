7db6da20f427d25615383d648074cf1afc9d6d2c
unknown <quynhnhuctcn@gmail.com>
2021-05-22 14:03:43 +0700

    giaodien

--- keystone/giaodien/components/Sach/Item/UI.js
+++ keystone/giaodien/components/Sach/Item/UI.js
@@ -25,61 +25,69 @@ import {
   Icon,
   chakra,
   Tooltip,
-} from '@chakra-ui/react';
-import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
-import { FiShoppingCart } from 'react-icons/fi';
+} from "@chakra-ui/react";
+import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
 
 const data = {
   isNew: true,
   imageURL:
-    'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
-  name: 'Wayfarer Classic',
+    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+  name: "Wayfarer Classic",
   price: 4.5,
   rating: 4.2,
   numReviews: 34,
 };
 
-
-
 function Rating({ rating, numReviews }) {
   return (
     <Box d="flex" alignItems="center">
       {Array(5)
-        .fill('')
+        .fill("")
         .map((_, i) => {
           const roundedRating = Math.round(rating * 2) / 2;
           if (roundedRating - i >= 1) {
             return (
               <BsStarFill
                 key={i}
-                style={{ marginLeft: '1' }}
-                color={i < rating ? 'teal.500' : 'gray.300'}
+                style={{ marginLeft: "1" }}
+                color={i < rating ? "teal.500" : "gray.300"}
               />
             );
           }
           if (roundedRating - i === 0.5) {
-            return <BsStarHalf key={i} style={{ marginLeft: '1' }} />;
+            return <BsStarHalf key={i} style={{ marginLeft: "1" }} />;
           }
-          return <BsStar key={i} style={{ marginLeft: '1' }} />;
+          return <BsStar key={i} style={{ marginLeft: "1" }} />;
         })}
-      <Box as="span" ml="2" color="gray.600" fontSize="sm">
-        {numReviews} review{numReviews > 1 && 's'}
-      </Box>
     </Box>
   );
 }
 
-function UI({sach}) {
+// bg={"green.300"}
+// href={"#"}
+// _hover={{
+//   bg: "green.200",
+// }}
+
+function UI({ sach }) {
   console.log(sach?.IMG[0]?.anh);
   return (
     <Flex p={50} w="full" alignItems="center" justifyContent="center">
       <Box
-        bg={useColorModeValue('white', 'gray.800')}
-        maxW="sm"
+        bg={useColorModeValue("white", "#F687B3")}
+        maxW="250px"
+        borderColor ="white"
         borderWidth="1px"
-        rounded="lg"
+         rounded="lg"
         shadow="lg"
-        position="relative">
+        position="relative"
+        href={"#"}
+        _hover={{
+          borderColor : "#F687B3",
+          borderWidth: "1px",
+        }}
+      >
         {data.isNew && (
           <Circle
             size="10px"
@@ -98,9 +106,9 @@ function UI({sach}) {
 
         <Box p="6">
           <Box d="flex" alignItems="baseline">
-              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="green">
-               {sach.tenTacGia}
-              </Badge>
+            <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="green">
+              {sach.tenTacGia}
+            </Badge>
           </Box>
           <Flex mt="1" justifyContent="space-between" alignContent="center">
             <Box
@@ -108,26 +116,36 @@ function UI({sach}) {
               fontWeight="semibold"
               as="h4"
               lineHeight="tight"
-              isTruncated>
+              isTruncated
+            >
               {sach.tenSach}
             </Box>
             <Tooltip
               label="Thêm vào giỏ"
               bg="white"
-              placement={'top'}
-              color={'gray.800'}
-              fontSize={'1.2em'}>
-              <chakra.a href={'#'} display={'flex'}>
-                <Icon as={FiShoppingCart} h={7} w={7} alignSelf={'center'} />
+              placement={"top"}
+              color={"gray.800"}
+              fontSize={"1.2em"}
+            >
+              <chakra.a href={"#"} display={"flex"}>
+                <Icon
+                  as={FiShoppingCart}
+                  h={7}
+                  w={7}
+                  color={"#D53F8C"}
+                  alignSelf={"center"}
+                />
               </chakra.a>
             </Tooltip>
           </Flex>
 
           <Flex justifyContent="space-between" alignContent="center">
-            <Rating rating={data.rating} numReviews={data.numReviews} />
-            <Box fontSize="2xl" color={useColorModeValue('gray.800', 'white')}>
-              <Box as="span" color={'gray.600'} fontSize="lg">
-                đ
+            <Box as="span" ml="2" color="#ECC94B">
+              <Rating rating={data.rating} />
+            </Box>
+            <Box fontSize="2xl" color={useColorModeValue("#ED64A6", "white")}>
+              <Box as="span" color={"#ED64A6"} fontSize="lg">
+                ₫
               </Box>
               {new Intl.NumberFormat().format(sach.gia)}
             </Box>
