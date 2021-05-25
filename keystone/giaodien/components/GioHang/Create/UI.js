6ea2dccab9848b4957582caae87cbac69cbfe345
tramdangthuy <tramdangthuy3011@gmail.com>
2021-05-25 11:06:51 +0700

    update

--- keystone/giaodien/components/GioHang/Create/UI.js
+++ keystone/giaodien/components/GioHang/Create/UI.js
@@ -4,8 +4,7 @@ export default function UI({CartItems, onCreate, resultCreateCart }) {
   return (
     <div>
       <button onClick={onCreate}>Tăng</button>
-      {resultCreateCart?.error && HTML.generation(resultCreateCart.error, 4)}
-      {resultCreateCart?.data && HTML.generation(resultCreateCart.data, 4)}
+      
     </div>
   );
 }
